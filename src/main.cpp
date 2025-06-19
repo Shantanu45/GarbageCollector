593248fae42c3a6dea2694a14d67b7e560dbcb8c
Shantanu Kumar <skumar@climaxstudios.com>
2025-06-19 12:15:04 +0100

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include <array>
+
 #include <iostream>
 #include <memory>
 
 #include "MemoryManager/MemoryManager.h"
-#include "MemoryManager/ObjectHeader.h"
 #include "Allocators/FreeList/FreeList.h"
 #include "Value/Value.h"
-#include "Utils/number-util.h"
 
 #define log(title, value) std::cout << title << " " << value << std::endl
 #define log(text) std::cout << text << std::endl
@@ -25,14 +23,6 @@ int main()
 	mm->writeValue(p2, Value::Number(2));
 	mm->writeValue(p1 + 1, Value::Pointer(p2));
 
-	log("p1 Number", mm->readValue(p1)->decode());
-
 	mm->free(p2);
 	mm->free(p1);
-
-
-	// alloc unreachable
-	//mm->writeValue(mm->allocate(4), Value::Number(3));
-	//mm->writeValue(mm->allocate(4), Value::Number(4));
-	//mm->writeValue(mm->allocate(4), Value::Number(5));
 } 
